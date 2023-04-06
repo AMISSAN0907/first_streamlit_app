52792a6834e755256140135b373c983564de7db7
AMISSAN0907 <130081388+AMISSAN0907@users.noreply.github.com>
2023-04-06 13:59:32 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
 streamlit.header('Breakfast Menu')
-streamlt.text('blueberry Oatmeal')
-streamlt.text('hard boiled free range egg')
+streamlit.text('blueberry Oatmeal')
+streamlit.text('hard boiled free range egg')
