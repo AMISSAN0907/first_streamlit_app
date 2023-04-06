465e36ea842d874d7c25d270cd1f560cd75eb170
AMISSAN0907 <130081388+AMISSAN0907@users.noreply.github.com>
2023-04-06 13:58:38 -0400

    Update streamlit_app.py
    j'ai rajouté le menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,5 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
+streamlit.header('Breakfast Menu')
+streamlt.text('blueberry Oatmeal')
+streamlt.text('hard boiled free range egg')
